29b879a828094d134bbbc14090824a7b3d4dcadd
alpink <kevinalpin@gmail.com>
2020-05-14 15:22:45 +0100

    New layout single column

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request 
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -17,8 +17,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
-
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -26,12 +24,18 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    session['user']=""
     return render_template("index.html", code="")
 
 @app.route("/register")
 def register():
     #userlist = db.execute("SELECT * FROM users;").fetchall()
     return render_template("register.html")
+
+@app.route("/logoff")
+def logoff():
+    session['user']=""
+    return render_template("index.html", code="")
 
 
 @app.route("/logon", methods=["POST"])
@@ -40,12 +44,14 @@ def logon():
     pwd = request.form.get("password")
     sqlString = f"SELECT * FROM users WHERE username= '{name}';"
     userrecord = db.execute(sqlString).fetchone()
-    if name == userrecord.username and pwd == userrecord.password:
-        session['user'] = name
-        return render_template("logon.html", person=userrecord, user=name,password=pwd)
+    if userrecord is not None:
+        if name == userrecord.username and pwd == userrecord.password:
+            session['user'] = name
+            return render_template("main.html")
+        else:
+            return render_template("index.html", code=404)
     else:
         return render_template("index.html", code=404)
-
 
 @app.route("/adduser", methods=["POST"])
 def adduser():
